20740242860eabdd14a7f64598dc37367bc783c5
thesrock <srock2023@my.fit.edu>
2025-10-25 13:25:54 -0400

    Gave basic directive header and outline for program

--- quizgame.asm
+++ quizgame.asm
@@ -1,1 +1,18 @@
 ; Quiz Game
+.386
+.model flat, stdcall
+.stack 4096
+
+; questions and answers
+.data
+
+
+; main program
+.code
+; Display question and possible answers
+
+
+; Prompt answer
+
+
+; Check answer, if wrong end gane, if correct move on to next question
