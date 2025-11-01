041620707387e000e0e2d1e7069323600695a1b4
thesrock <srock2023@my.fit.edu>
2025-10-31 23:52:28 -0400

    Added a starting message
    added the main PROC properly and added startGame and WaitMsg prior to the game start

--- quizgame.asm
+++ quizgame.asm
@@ -2,13 +2,21 @@
 .386
 .model flat, stdcall
 .stack 4096
+INCLUDE Irvine32.inc
 
 ; questions and answers
 .data
-
+startGame BYTE "Ready to start the Assembly Quiz Game?", 0
 
 ; main program
 .code
+main PROC
+  mov edx, OFFSET startGame
+  call WriteString   ; print out startGame message
+  call Crlf
+  call WaitMsg   ; wait for user to start with enter
+  call Crlf
+
 ; Display question and possible answers
 
 
@@ -16,3 +24,6 @@
 
 
 ; Check answer, if wrong end gane, if correct move on to next question
+
+main ENDP
+END main
